807af057f51090e745c058b22a9086007c6898f4
Serg0501 <gavrilenko87sd@gmail.com>
2022-11-06 12:58:25 +0200

    send msg

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,18 +4,17 @@ const socket = new WebSocket('ws://localhost:3000/');
 
 socket.onopen = function (event){
   console.dir(event);
-  alert('WS-Connection is open your mind!!!!')
-};
+  alert('WS-Connection is opened your mind!!!!!');
+}
 
 socket.onclose = function (event){
   console.dir(event);
-  alert('WS-Connection is close your mind!!!!')
-};
+  alert('WS-Connection is closed your mind!!!!');
+}
 
 socket.onmessage = function(event){
   console.dir(event.data);
-};
-
+}
 
 class App extends Component{
   constructor(props){
@@ -25,7 +24,7 @@ class App extends Component{
       msgText: '',
     };
   }
-  handleMsg = (e) => {
+  handleMsg = (e) => { 
     this.setState({msgText: e.target.value});
   };
   sendMsg = (e) => {
--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -3,18 +3,27 @@ const express = require("express");
 const app = express();
 const expressWS = require('express-ws') (app);
 
+// отправка для всех
+const allWS = expressWS.getWss('/');
+
 app.use(express.json());
 
 app.ws('/', function(ws,req){
-    //слушает клиента (сообщение)
-    ws.on('message', (msg) => {console.log(msg)
-    //ws.send(msg);
-});
+    // слушает клиента (сообщение)
+    ws.on('message', function(msg){
+        console.log(msg); 
+        ws.send(msg);
+        // all
+        allWS.clients.forEach(function(client){
+            client.send(`b: ${msg}`);
+        });
+    });
     console.log('socket connection', req.testing);
-    console.log('socket request', req.testing);
+
 });
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '127.0.0.1';
 
-app.listen(PORT, HOST, () => console.log(`App listening on port ${PORT} on ${HOST}`));
+app.listen(PORT, HOST, 
+    () => console.log(`App listening on port ${PORT} on ${HOST}`));
